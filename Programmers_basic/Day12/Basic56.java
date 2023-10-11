dbc7f574851a800c04ad9297deb357c6cb4cc83f
Yesol Son <tosori@naver.com>
2023-10-11 21:59:12 +0900

    2023.10.11 코딩테스트기초 Day13

--- Programmers_basic/Day12/Basic56.java
+++ Programmers_basic/Day12/Basic56.java
@@ -1,4 +1,42 @@
 package Programmers_basic.Day12;
+//리스트 자르기
+import java.util.ArrayList;
+import java.util.List;
 
 public class Basic56 {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> arr = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+
+        switch(n){
+            case 1:
+                for(int i=0; i<=b;i++){
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for(int i=a; i<num_list.length;i++){
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for(int i=a; i<=b;i++){
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 4:
+                for(int i=a; i<=b; i+=c){
+                    arr.add(num_list[i]);
+                }
+                break;
+        }
+
+        int[] answer = new int[arr.size()];
+        for(int i=0; i<arr.size();i++){
+            answer[i]=arr.get(i);
+        }
+        return answer;
+    }
 }
--- Programmers_basic/Day12/Basic60.java
+++ Programmers_basic/Day12/Basic60.java
@@ -1,4 +1,28 @@
 package Programmers_basic.Day12;
+//배열 조각하기
+import java.util.ArrayList;
 
 public class Basic60 {
+    public int[] solution(int[] arr, int[] query) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int num: arr){
+            list.add(num);
+        }
+
+        for(int i=0; i<query.length; i++){
+            if(i%2==0){
+                list.remove(query[i]+1);
+            }else if(i%2==1){
+                list.remove(query[i]-1);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size();i++){
+            answer[i]= list.get(i);
+        }
+
+        return answer;
+    }
+
 }
