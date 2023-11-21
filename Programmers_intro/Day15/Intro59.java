3ccc8dd12f542511b6cf39a18fb7ca520b9f0911
Yesol Son <tosori@naver.com>
2023-11-21 12:28:38 +0900

    2023.11.21 코딩테스트입문 Day15

--- Programmers_intro/Day15/Intro59.java
+++ Programmers_intro/Day15/Intro59.java
@@ -1,4 +1,36 @@
 package Programmers_intro.Day15;
+//한 번만 등장한 문자
+
+import java.util.Arrays;
 
 public class Intro59 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] str = s.split("");
+
+        Arrays.sort(str);
+
+
+        for(int i=0;i<str.length;i++){
+            String word = str[i];
+            int res=0;
+            for(int j=0;j<str.length;j++){
+                if(str[j].equals(word)){
+                    res+=1;
+                }else{
+                    continue;
+                }
+
+            }
+            if(res==1){
+                answer+=str[i];
+            }else{
+                continue;
+            }
+
+        }
+
+        return answer;
+    }
 }
--- Programmers_intro/Day15/Intro60.java
+++ Programmers_intro/Day15/Intro60.java
@@ -1,4 +1,23 @@
 package Programmers_intro.Day15;
+//약수 구하기
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Intro60 {
+    public int[] solution(int n) {
+
+        List<Integer> arr = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                arr.add(i);
+            }
+        }
+
+        int[] answer = new int[arr.size()];
+        for(int i=0;i<arr.size();i++){
+            answer[i]=arr.get(i);
+        }
+        return answer;
+    }
 }
