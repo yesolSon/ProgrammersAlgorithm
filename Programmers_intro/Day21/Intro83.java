abcefd7d5f87c82bfb3b88de3462cee8b2e86b9c
Yesol Son <tosori@naver.com>
2023-11-29 13:06:24 +0900

    2023.11.29 코딩테스트입문 Day21

--- Programmers_intro/Day21/Intro83.java
+++ Programmers_intro/Day21/Intro83.java
@@ -1,4 +1,20 @@
 package Programmers_intro.Day21;
+//삼각형의 완성조건
 
 public class Intro83 {
+    public int solution(int[] sides) {
+        int answer = 0;
+
+        int max = Math.max(sides[0],sides[1]);
+        int min = Math.min(sides[0],sides[1]);
+
+        int highRange = max+min;
+        int lowRange = max-min;
+
+        answer = highRange-lowRange-1;
+
+
+
+        return answer;
+    }
 }
--- Programmers_intro/Day21/Intro84.java
+++ Programmers_intro/Day21/Intro84.java
@@ -1,4 +1,20 @@
 package Programmers_intro.Day21;
+//외계어 사전
 
 public class Intro84 {
+    public int solution(String[] spell, String[] dic) {
+        boolean wordCheck = false;
+
+        for (String word : dic) {
+            int count = 0;
+            for (String s : spell) {
+                if (word.contains(s)) count++;
+            }
+            if (count == spell.length) {
+                wordCheck = true;
+                break;
+            }
+        }
+        return wordCheck ? 1 : 2;
+    }
 }
